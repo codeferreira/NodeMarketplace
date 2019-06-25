60f941d09bde77f03b8e8787e8d3eabc75ab60d7
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-06-25 19:25:12 -0300

    Routes
    - Create user
    - Create a Session

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const routes = express.Router()
 
+const UserController = require('./app/controller/UserController')
+const SessionController = require('./app/controller/SessionController')
+
+routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
+
 module.exports = routes
