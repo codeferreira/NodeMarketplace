8d25c07ed6f0feffe7b87bcba727eafe7cc03990
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-06-27 17:54:45 -0300

    New Routes for Ads and Purchases
    - Ad's CRUD
    - Purchase Solicitation and Confirm

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const validate = require('express-validation')
+const handle = require('express-async-handler')
+
 const routes = express.Router()
 
-const UserController = require('./app/controller/UserController')
-const SessionController = require('./app/controller/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+const controllers = require('./app/controller')
+const validators = require('./app/validators')
+
+routes.post('/users', validate(validators.User), handle(controllers.UserController.store))
+routes.post('/sessions', validate(validators.Session), handle(controllers.SessionController.store))
+
+routes.use(authMiddleware)
+
+routes.get('/ads', handle(controllers.AdController.index))
+routes.get('/ads/:id', handle(controllers.AdController.show))
+routes.post('/ads', validate(validators.Ad), handle(controllers.AdController.store))
+routes.put('/ads/:id', validate(validators.Ad), handle(controllers.AdController.update))
+routes.delete('/ads/:id', handle(controllers.AdController.destroy))
+
+routes.post('/purchases', validate(validators.Purchase), handle(controllers.PurchaseController.store))
+routes.put('/purchases/:id', handle(controllers.ApproveController.update))
 
 module.exports = routes
